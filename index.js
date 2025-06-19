efaa01d7ff197f1075fe127f67b742cd14f2657c
Marcelo Muniz <marceloocara01@gmail.com>
2025-06-19 12:55:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,18 @@
-
-
 const express = require('express');
-const cors = require('cors');
+const conexao = require('./infra/conexao');
+const router = require('./routers/index');
+
+// versão do node: 22.11.0
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+const porta = 3000;
 
-const port = process.env.PORT || 3001;
+router(app,express);
 
-const users = [];
-
-app.get('/',(req,res) => {
-    return res.json('Hello world');
-});
-
-app.get('/users',(req,res) => {
-    return res.json(users);
-});
-
-app.post('/users',(req,res) => {
-    const { name, email } = req.body;
-
-    const newUser = {
-        id:Math.random().toString(36),
-        name,
-        email
-    };
-
-    users.push(newUser);
-    return res.json(newUser);
-});
-
-app.delete('/users/:id',(req,res) => {
-    const { id } = req.params;
-    return res.json(id);
-})
-
-app.listen(port,(error) => {
-    if (error) {
-        console.log('Erro ao iniciar servidor express!');
-        console.log(error.log);
-    }
-
-    console.log(`Servidor rodando na porta ${port}`);
+app.listen(porta,(error) => {
+	if (error){
+		console.log('Erro ao iniciar servidor express');
+		console.log(error.message);
+	}
+	console.log(`Servidor iniciado com sucesso na porta ${porta}`);
 })
